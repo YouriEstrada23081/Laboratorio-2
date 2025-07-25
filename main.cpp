f54b8ee0e9ca2f15e01eac303e158f6bc2aeb5eb
YouriEstrada23081 <est23081@uvg.edu.gt>
2025-07-24 21:40:45 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,105 @@
+//*************************************/
+// Universidad del Valle de Guatemala
+// BE30239 Electronica digital 2
+// Youri Estrada 
+// Laboratorio 2
+// MCU: Esp32 dev kit 1.0
+//************************************/
+
+#include <Arduino.h>
+
+#define LED1 33
+#define LED2 25
+#define LED3 26
+#define LED4 27
+
+#define bt1 2
+#define bt2 17
+#define bt3 18
+
+int contador=0;
+unsigned long ts = 0;
+unsigned long tr = 0;
+const long d = 300; 
+
+bool us2= false;
+bool us3=false;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+  pinMode(LED4, OUTPUT);
+
+  pinMode(bt1,INPUT);
+  pinMode(bt2, INPUT_PULLDOWN);
+  pinMode(bt3,INPUT_PULLUP);
+
+  Serial.begin(115200);
+
+}
+
+void loop() {
+  bool b2=digitalRead(bt2);
+  bool b3=digitalRead(bt3);
+  
+  if (b2 == true && us2 == false) { 
+    if ((millis() - ts) > d) { 
+      contador++;
+      ts = millis();
+    }
+  }
+  us2=b2;
+
+  if (b3 == true && us3 == false) { 
+    if ((millis() - tr) > d) { 
+      contador--;
+      tr = millis();
+    }
+  }
+  us3=b3;
+  if (contador==-1){
+    contador=4;
+  }
+  if (contador==5){
+    contador=0;
+  }
+  switch (contador)
+  {
+  case 0:
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, LOW);
+    digitalWrite(LED4, LOW);
+    break;
+  case 1: 
+    digitalWrite(LED1, HIGH);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, LOW);
+    digitalWrite(LED4, LOW);
+    break;
+  case 2:
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED2, HIGH);
+    digitalWrite(LED3, LOW);
+    digitalWrite(LED4, LOW);
+    break;
+  case 3: 
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, HIGH);
+    digitalWrite(LED4, LOW);
+    break;
+  case 4: 
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, LOW);
+    digitalWrite(LED4, HIGH);
+    break;
+  }
+
+Serial.println(contador);
+
+
+}
